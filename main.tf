8baac5bdf796d765afd5c071c81a038b15884e62
Eugene Crisostomo <56553426+eccris@users.noreply.github.com>
2021-08-18 22:06:41 -0400

    Removed EC2 Instance

--- main.tf
+++ main.tf
@@ -26,13 +26,4 @@ resource "aws_instance" "ubuntu" {
     Name                 = var.instance_name
     "Linux Distribution" = "Ubuntu"
   }
-  
-  resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name                 = "Eugene2"
-    "Linux Distribution" = "Ubuntu"
-  }
 }
