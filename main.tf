738d965c0e0723b5368b35e57a8586130327c5c7
Eugene Crisostomo <56553426+eccris@users.noreply.github.com>
2021-08-18 22:08:37 -0400

    Added Eugene2 EC2 instance

--- main.tf
+++ main.tf
@@ -27,3 +27,13 @@ resource "aws_instance" "ubuntu" {
     "Linux Distribution" = "Ubuntu"
   }
 }
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name                 = "Eugene2"
+    "Linux Distribution" = "Ubuntu"
+  }
+}
